44ec27b9636bd62e0ab1b330c3384b4118ba847d
Joseph Gentle <me@josephg.com>
2013-10-18 17:49:01 -0700

    Added example nodejs server

---
+++ nodejs/README.md
@@ -0,0 +1,10 @@
+# NodeJS example jobs server
+
+This is an example job posting site written using NodeJS & Express. To run it,
+first install [nodejs](http://nodejs.org/) then run `npm install` from this
+directory.
+
+I haven't used a templating engine in this example to keep it simple, but if
+you want to build from this example, you should definitely pick one and use it.
+[Jade](http://jade-lang.com/) is quite popular.
+
---
+++ nodejs/server.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var request = require('request');
+
+// Change this to your postings site shortname.
+var SITE = 'lever';
+
+var BASE = 'https://api.lever.co/v0/postings/' + SITE;
+
+app = express();
+
+// At the root page, we'll display all postings.
+app.get('/', function(req, res, next) {
+  // Get postings from REST api.
+  request({url:BASE, json:true}, function(err, r, postings) {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    // You should use a templating engine to render the postings list.
+    var page = "<!DOCTYPE html>\n";
+    page += "<h1>Come work at my rad company!</h1>\n"
+
+    for (var i = 0; i < postings.length; i++) {
+      var posting = postings[i];
+
+      page += "<div>";
+      page += "<a href='" + posting.id + "'>" + posting.text + "</a>";
+      page += "</div>\n";
+    }
+    res.send(page);
+  });
+});
+
+// Display the named posting
+app.get('/:id', function(req, res, next) {
+  // Get the specified posting
+  var id = req.params.id;
+  request({url:BASE + '/' + id, json:true}, function(err, r, posting) {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    // If the posting doesn't exist (or some other error occurs), we'll forward
+    // the error to our user.
+    if (r.statusCode !== 200) {
+      res.send(r.statusCode);
+      return;
+    }
+
+    // Render the posting. Again, you should use a templating engine to render
+    // this.
+  
+    // There's a few more fields available in the posting object that I'm not
+    // using here including location, team, commitment, tags and posting date.
+    var page = "<!DOCTYPE html>\n";
+    page += "<title>" + posting.text + "</title>\n";
+    page += "<h1>" + posting.text + "</h1>\n";
+    page += "<p>" + posting.description + "</p>\n";
+    for (var i = 0; i < posting.lists.length; i++) {
+      var list = posting.lists[i];
+      page += "<p>" + list.text + "</p>\n";
+      page += "<div>" + list.content + "</div>\n";
+      page += "<br>";
+    }
+
+    if (posting.additional) {
+      page += "<p>" + posting.additional + "</p>\n";
+    }
+
+    page += "<a href='" + posting.applyUrl + "'>Apply now!</a>\n";
+
+    res.send(page);
+  });
+});
+
+port = process.env.PORT || 5000;
+app.listen(port);
+console.log('Listening on http://localhost:' + port + '/');
